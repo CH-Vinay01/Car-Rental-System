da650ad414ab3e2961404d6fc2bda9a63284ab0f
CH-Vinay01 <chvinayreddyabc@gmail.com>
2025-10-14 09:53:06 +0530

    Localhost:5173 permission to api is established successfully

--- src/main/java/in/chapparapuvinay/carrentalsystem/Controller/CarController.java
+++ src/main/java/in/chapparapuvinay/carrentalsystem/Controller/CarController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/cars")
 @AllArgsConstructor
